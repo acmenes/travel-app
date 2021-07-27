182b71b4775fcffdfb9f3f1b76bb30600f563160
acmenes <acmenescompositions@gmail.com>
2021-07-27 10:56:08 -0400

    fixing layout on country page

--- static/app.js
+++ static/app.js
@@ -10,28 +10,24 @@ const $addBeenThere = document.getElementById("add-been-there")
 
 function handleClick(e) {
     e.preventDefault()
-    processForm()
+    // processForm()
 }
 
-async function processForm(searchterm) {
-    countrySearched = document.getElementById("country-search").value
-    console.log(countrySearched)
-    const res = await axios.get(`${BASE_URL_COUNTRIES}/${countrySearched}`)
-    country = res.data[0]
-    console.log(res.data[0])
-    console.log(`You searched for ${country.name}`)
-    console.log(country.borders)
-    console.log(`The capital of ${country.name} is ${country.capital}`)
-    console.log(`Languages spoken are ${country.languages[0]}`)
-    console.log(`${country.name} is in this timezone: ${country.timezones}`)
-    console.log(country.flag)
-}
-
-/// I need to serialize this data into JSON so I can feed it to the app and then use jinja
+// async function processForm(searchterm) {
+//     countrySearched = document.getElementById("country-search").value
+//     console.log(countrySearched)
+//     const res = await axios.get(`${BASE_URL_COUNTRIES}/${countrySearched}`)
+//     country = res.data[0]
+//     console.log(res.data[0])
+//     console.log(`You searched for ${country.name}`)
+//     console.log(country.borders)
+//     console.log(`The capital of ${country.name} is ${country.capital}`)
+//     console.log(`Languages spoken are ${country.languages[0]}`)
+//     console.log(`${country.name} is in this timezone: ${country.timezones}`)
+//     console.log(country.flag)
+// }
 
 $("#search-countries").on("submit", handleClick);
-
-// $("#search-button").on("click", processForm)
 
 $("#add-dream-fest").on("submit", handleDreamDest)
 
